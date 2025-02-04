edb445b7c2c35415fcea0ccbaf708ead2e4b2c33
Daniel Pecher <dan.pecher@gmail.com>
2025-02-04 23:31:09 +0100

    Support dynamic fonts

--- startupjobs/Sources/Core/UI/Fonts.swift
+++ startupjobs/Sources/Core/UI/Fonts.swift
@@ -1,8 +1,20 @@
 import SwiftUI
 
 struct Fonts {
-    static let note = StartupJobsFontFamily.FunnelDisplay.light.swiftUIFont(size: 12)
-    static let regular = StartupJobsFontFamily.FunnelDisplay.regular.swiftUIFont(size: 14)
-    static let regularBold = StartupJobsFontFamily.FunnelDisplay.bold.swiftUIFont(size: 14)
-    static let title = StartupJobsFontFamily.FunnelDisplay.bold.swiftUIFont(size: 17)
+    static let note = Font.custom(
+        StartupJobsFontFamily.FunnelDisplay.light.name,
+        size: UIFontMetrics.default.scaledValue(for: 12)
+    )
+    static let regular = Font.custom(
+        StartupJobsFontFamily.FunnelDisplay.regular.name,
+        size: UIFontMetrics.default.scaledValue(for: 14)
+    )
+    static let regularBold = Font.custom(
+        StartupJobsFontFamily.FunnelDisplay.bold.name,
+        size: UIFontMetrics.default.scaledValue(for: 14)
+    )
+    static let title = Font.custom(
+        StartupJobsFontFamily.FunnelDisplay.bold.name,
+        size: UIFontMetrics.default.scaledValue(for: 17)
+    )
 }
